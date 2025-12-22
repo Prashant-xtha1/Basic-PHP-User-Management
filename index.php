ef3bc753e1b020b81a7091d6030325787988a88d
Prashant-xtha1 <prince.kakshapati@gmail.com>
2025-12-22 16:21:12 +0545

    New php files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>User Management</title>
+  <link rel= "stylesheet" href="./main.css">
+</head>
+<body>
+  <main class= "container">
+    <h1 class = "page-title">User Management</h1>
+    <div class= "text-container" >
+      <p>Welcome to user management system. Here you can manage user accounts, roles, and permissions.</p>
+    </div>
+    <section class= "list-box">
+      <h2 class= "section-title">Features</h2>
+      <ul>
+        <li>Create, read, update, and delete user accounts</li>
+        <li>Assign roles and permissions to users (Basic SESSION)</li>
+        <li>List all users</li>
+      </ul>
+    </section>
+    <div class= "btn-group">
+      <a href="login.php" class= "btn">Login Now</a>
+      <a href="register.php" class= "btn">Register Users</a>
+      <a href="list.php" class= "text-link">View All Users &rarr;</a>
+    </div>
+</body>
+</html>
